bc045210716d576297a328a00bdf7680c1bc16f3
behzad-robot <behzad.robot@gmail.com>
2024-05-08 21:04:12 +0330

    update docker-compose , env

--- ordering/env/env.go
+++ ordering/env/env.go
@@ -1,6 +1,12 @@
 package env
 
-import "github.com/spf13/viper"
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/spf13/viper"
+)
 
 type Env struct {
 	Port               int
@@ -9,12 +15,39 @@ type Env struct {
 }
 
 func CreateEnv() (*Env, error) {
-	viper.SetConfigFile(".env")
-	err := viper.ReadInConfig()
-	if err != nil {
-		return nil, err
+	_, e := os.Stat(".env")
+	if e == nil {
+		fmt.Println("reading env from .env file")
+		viper.SetConfigFile(".env")
+		err := viper.ReadInConfig()
+		if err != nil {
+			fmt.Println("Viper Error", err)
+			return nil, err
+		}
+		os.Setenv("PORT", viper.GetString("PORT"))
+		os.Setenv("POSTGRESQL_DATABASE", viper.GetString("POSTGRESQL_DATABASE"))
+		os.Setenv("RABBITMQ_CONNECTION", viper.GetString("RABBITMQ_CONNECTION"))
 	}
-	result := &Env{}
-	err = viper.Unmarshal(&result)
-	return result, err
+	// Read from environment variables
+	portStr := os.Getenv("PORT")
+	port, err := strconv.Atoi(portStr)
+	if err != nil {
+		port = 3000 // default port
+	}
+
+	db := os.Getenv("POSTGRESQL_DATABASE")
+	if db == "" {
+		return nil, fmt.Errorf("POSTGRESQL_DATABASE environment variable not set")
+	}
+
+	rabbitMQ := os.Getenv("RABBITMQ_CONNECTION")
+	if rabbitMQ == "" {
+		return nil, fmt.Errorf("RABBITMQ_CONNECTION environment variable not set")
+	}
+
+	return &Env{
+		Port:               port,
+		PostgresqlDatabase: db,
+		RabbitMQConnection: rabbitMQ,
+	}, nil
 }
